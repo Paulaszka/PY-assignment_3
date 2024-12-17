fd0aabb9093d91aff570a4ff4667e735842417f6
247798 Paulina Szczepaniak <247798@edu.p.lodz.pl>
2024-12-17 21:36:49 +0100

    new address

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ if __name__ == '__main__':
     # Creating tables in database if they do not exist
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5050)
+    app.run(host='127.0.0.1', port=5050)
